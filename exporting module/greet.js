1c3bd5f229ae2c714ad97da93a45c7286dc9124a
chrollolucilfer-v <guptabhaskar9897@gmail.com>
2026-06-09 22:44:04 +0530

    module exporting

---
+++ exporting module/greet.js
@@ -0,0 +1,13 @@
+function greet(username)
+{
+    console.log("hello", username);
+
+}
+
+// how can we use this function in other functions
+
+// tellin other files you can use this function
+
+module.exports = greet;
+
+// syntax for exporting a module or a function or somethin
---
+++ exporting module/index.js
@@ -0,0 +1,5 @@
+const greet = require('./greet')
+
+// this allows us to use the function form greet.js file,
+
+greet("bhaskar");
